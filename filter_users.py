9c21d91d11a055f08d060e8cb2a50352bc63b100
Jan <janreichenbach1991@gmail.com>
2026-08-24 17:00:31 +0200

    small improvenets to user choice - added age and email to user input

--- filter_users.py
+++ filter_users.py
@@ -15,7 +15,7 @@ def filter_users_by_age(age):
     with open("users.json", "r") as file:
         users = json.load(file)
 
-    filtered_users = [user for user in users if user.get("age") == age]
+    filtered_users = [user for user in users if user.get("age") == int(age)]
 
     for user in filtered_users:
         print(user)
@@ -33,10 +33,16 @@ def filter_users_by_email(email):
 
 
 if __name__ == "__main__":
-    filter_option = input("What would you like to filter by? (Currently, only 'name' is supported): ").strip().lower()
+    filter_option = input("What would you like to filter by? (name, age, email): ").strip().lower()
 
     if filter_option == "name":
         name_to_search = input("Enter a name to filter users: ").strip()
         filter_users_by_name(name_to_search)
+    elif filter_option == "age":
+        age_to_search = input("Enter an age to filter users: ").strip()
+        filter_users_by_age(age_to_search)
+    elif filter_option == "email":
+        email_to_search = input("Enter an email to filter users: ").strip()
+        filter_users_by_email(email_to_search)
     else:
         print("Filtering by that option is not yet supported.")
